52a8416046017c7fefe35712bc2f0b781f52b899
Dron1708 <andreyprikhod17@gmail.com>
2024-07-17 14:29:40 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+complete = 12
+hours = 1.5
+course = 'Python'
+hours_per_task = (hours / complete)
+print ('Курс:',course+',','всего задач:',str(complete)+',','затрачено часов:',str(hours)+',','среднее время выполнения:',str(hours_per_task)+'.')
