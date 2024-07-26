25689a966f861146d516c2470885fa56db48a2eb
Dron1708 <andreyprikhod17@gmail.com>
2024-07-26 11:13:50 +0500

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,30 @@
+calls = 0
+def count_calls(n):
+    global calls
+    calls += n
+def string_info(string):
+    lengh = len(string)
+    up = string.upper()
+    low = string.lower()
+    answer = (lengh, up, low)
+    count_calls(1)
+    return answer
+def is_contains(string, list_to_search):
+    s = string.lower()
+    list_ = []
+    answer2 = False
+    for i in list_to_search:
+        list_.append(i.lower())
+    if s in list_:
+        answer2 = True
+    count_calls(1)
+    return answer2
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+
+
+
+
