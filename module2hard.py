94c21866c2b03370157d6d05ef583801e3062fb0
Dron1708 <andreyprikhod17@gmail.com>
2024-07-26 10:15:23 +0500

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -1,10 +1,10 @@
 def numbers(answer):
     res = []
-    for i in range(1,20):
-        for j in range(1,20):
-            if answer % (i + j) == 0 and (i, j) not in res and (j, i) not in res and i != j:
+    for i in range(1,answer):
+        for j in range(i+1,answer+1):
+            if answer % (i + j) == 0 and (i, j) not in res and (j, i) not in res:
                 res.append((i, j))
 
     return res
 res = numbers(int(input('ввеи число от 3 до 20 ')))
-print(res)
+print(*res)
