2631d4990eda04cbd2e62c18bcd755999b976116
Dron1708 <andreyprikhod17@gmail.com>
2024-07-26 11:16:54 +0500

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -12,11 +12,12 @@ def string_info(string):
 def is_contains(string, list_to_search):
     s = string.lower()
     list_ = []
-    answer2 = False
     for i in list_to_search:
         list_.append(i.lower())
     if s in list_:
         answer2 = True
+    else:
+        answer2 = False
     count_calls(1)
     return answer2
 print(string_info('Capybara'))
@@ -24,7 +25,6 @@ print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
 print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
 print(calls)
-
 
 
 
